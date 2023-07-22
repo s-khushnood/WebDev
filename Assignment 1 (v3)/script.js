bfcd8c7967e4a2a8353190e98717a98a6e805ff3
s khushnood <138812257+s-khushnood@users.noreply.github.com>
2023-07-22 16:14:35 +0500

    Add files via upload

--- Assignment 1 (v3)/script.js
+++ Assignment 1 (v3)/script.js
@@ -1,4 +1,36 @@
+var imgs=document.getElementsByTagName("img");
+var dots=document.getElementsByClassName("dot");
+var curimg=0;
+var interval=2000;
+var timer=setInterval(dotchange,interval)
+
+function dotchange(n){
+   for (var i=0;i<imgs.length;i++)
+   {
+      imgs[i].style.opacity=0;
+   }
+   
+   curimg=(curimg+1)%imgs.length;
+   // if(n!=undefined)
+   // {clearInterval(timer);
+   // timer=setInterval(dotchange,interval)
+   // curimg=n;
+   // }
+   imgs[curimg].style.opacity=1;
+}
+
+
+
+
+
+
+
 function services(){
     document.getElementById("introduction").style.display="none";
     document.getElementById("service").style.display="block";
  }
+
+ function table(){
+    document.getElementById("mytable").style.display="block";
+ }
+
