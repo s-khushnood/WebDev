84b23a62c12ab1598b821fc77c542cff5c95d60f
s khushnood <138812257+s-khushnood@users.noreply.github.com>
2023-07-24 06:53:02 -0400

    Add files via upload

--- Assignment 1 (v3)/supportfiles/script.js
+++ Assignment 1 (v3)/supportfiles/script.js
@@ -1,53 +1,67 @@
+//Mouse Hover for nav links
+var link = document.querySelectorAll(".nav a");
+for (let i = 0; i < link.length; i++) {
+   link[i].addEventListener("mouseover", colchange);
+   link[i].addEventListener("mouseout", orgcol);
+ 
+}
+
+function colchange() {
+   this.style.backgroundColor = "grey";
+
+}
+function orgcol() {
+   this.style.backgroundColor = "";
+}
+
+
+
+
 //function to change images using buttons
 var imgs = document.getElementsByTagName("img");
-var curimg = 0;
+var curimg = 3;
 
 function imagechange(n) {
-   for (var i = 0; i < imgs.length; i++) {
+   for (let i = 0; i < imgs.length; i++) {
       imgs[i].style.opacity = 0;
    }
-   if(n===1)
-   {
-      curimg =curimg+1;
-      if(curimg>3)
-      {
-         curimg=0;
+   if (n === 1) {
+      curimg = curimg + 1;
+      if (curimg > 3) {
+         curimg = 0;
       }
-     
+
       imgs[curimg].style.opacity = 1;
-     
+
    }
 
-   else if(n===2)
-   {
-      curimg =curimg-1;
-      if(curimg<0)
-      {
-         curimg=3;
+   else if (n === 2) {
+      curimg = curimg - 1;
+      if (curimg < 0) {
+         curimg = 3;
       }
-     
+
       imgs[curimg].style.opacity = 1;
-     
+
    }
-   
+
 }
 
 
 
 //function to show demo and change button contents
-var j=0;
+let j = 0;
 function showdemo() {
-   if(j===0)
-   {
-      document.getElementById("div3demo").style.display = "block";
-      j=j+1;
-      document.querySelector(".div3 button").innerHTML = 'Hide';
-   }
-   else if(j===1)
-   {
+   if (j === 0) {
       document.getElementById("div3demo").style.display = "none";
-      j=0;
+      j = j + 1;
       document.querySelector(".div3 button").innerHTML = 'Show Demo';
+
+   }
+   else if (j === 1) {
+      document.getElementById("div3demo").style.display = "block";
+      j = 0;
+      document.querySelector(".div3 button").innerHTML = 'Hide Demo';
    }
 }
 
@@ -74,11 +88,11 @@ function buttonblink() {
 //function to add data into table
 let k = 4;
 function addrow() {
- var Inputs=document.querySelectorAll(".demo input");
- var invest=Inputs[0].value;
- var ROI=Inputs[1].value;
- var Prd=Inputs[2].value;
-   if (invest.length === 0 || ROI.length === 0 || Prd.length === 0 || isNaN(invest) || isNaN(ROI) || isNaN(Prd)||invest<0||ROI<0||Prd<0) {
+   var Inputs = document.querySelectorAll(".demo input");
+   var invest = Inputs[0].value;
+   var ROI = Inputs[1].value;
+   var Prd = Inputs[2].value;
+   if (invest < 0 || ROI < 0 || Prd < 0 || invest === "" || ROI === "" || Prd === "") {
       alert("Incomplete/Invalid data!!!");
    }
    else {
@@ -93,12 +107,12 @@ function addrow() {
       cell1.innerHTML = invest;
       cell2.innerHTML = ROI;
       cell3.innerHTML = Prd;
-      var Result = parseFloat((invest)) * (parseFloat(1) + parseFloat(ROI) /parseFloat(100)) ^ parseFloat(Prd);
+      var Result = parseFloat((invest)) * (parseFloat(1) + parseFloat(ROI) / parseFloat(100)) ^ parseFloat(Prd);
       cell4.innerHTML = Result;
 
-      Inputs[0].value="";
- Inputs[1].value="";
- Inputs[2].value="";
+      Inputs[0].value = "";
+      Inputs[1].value = "";
+      Inputs[2].value = "";
 
 
       k = k + 1;
@@ -108,17 +122,21 @@ function addrow() {
 }
 
 //function to change nav bar color with scroll
-window.onscroll=function(){navcolorchange()};
+window.onscroll = function () { navcolorchange() };
 
 function navcolorchange() {
    if (document.documentElement.scrollTop > 400) {
-     document.querySelector(".nav").style.backgroundColor = "darkslategray";
+      document.querySelector(".nav").style.backgroundColor = "darkslategray";
    }
    else if (document.documentElement.scrollTop < 400) {
       document.querySelector(".nav").style.backgroundColor = "black";
-    }
+   }
 
- }
+}
 
-
+function formsubmit(event){
+event.preventDefault();
+ document.getElementById("msg").innerHTML="Your Data Submitted. We will try to reach you as soon as possible!!"
+ console.log("yeah");
+}
 
